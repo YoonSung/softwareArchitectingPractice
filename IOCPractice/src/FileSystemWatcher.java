f56f39e1c11a14e2e6f55d1b5958c2596c312860
Yoonsung <lvev9925@naver.com>
2014-07-17 02:57:41 +0900

    Refactoring

--- IOCPractice/src/FileSystemWatcher.java
+++ IOCPractice/src/FileSystemWatcher.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,34 +7,73 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
-
-public class FileSystemWatcher {
+public class FileSystemWatcher extends Thread{
 	
 	private String WATCH_DIRECTORY = "." + File.separator + "etc";
 	private String WATCH_FILE = "setting.xml";
 	
-	public FileSystemWatcher() throws IOException, InterruptedException {
+	private boolean isOperation = true;
+	
+	private static FileSystemWatcher instance = new FileSystemWatcher();
+	
+	private FileSystemWatcher(){}
+	
+	static FileSystemWatcher getInstance() {
+		return instance;
+	}
+	
+	void watchStop() {
+		isOperation = false;
+	}
+	
+	boolean isOperation() {
+		return isOperation;
+	}
+	
+	boolean watchStart() {
+		boolean isSuccess = true;
+		
 		Path path = Paths.get(WATCH_DIRECTORY);
 		
-		final WatchService watchService = FileSystems.getDefault().newWatchService();
-		final WatchKey watchKey = path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
-		while (true) {
-		    final WatchKey wk = watchService.take();
-		    for (WatchEvent<?> event : wk.pollEvents()) {
-
-		        final Path changed = (Path) event.context();
-		        System.out.println("Path modify detected!! : " + changed);
-		        if (changed.endsWith(WATCH_DIRECTORY + File.separator + WATCH_FILE)) {
-		            System.out.println(WATCH_DIRECTORY + File.separator + WATCH_FILE + " Changed");
-		            //TODO
-		        }
-		    }
-		    
-		    // Reset the key
-		    boolean valid = wk.reset();
-		    if (!valid) {
-		        System.out.println("Key has been unregisterede");
-		    }
-		}
+		WatchService watchService;
+		try {
+			watchService = FileSystems.getDefault().newWatchService();
+			//final WatchKey watchKey = path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+			path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+			
+			while (isOperation) {
+				final WatchKey wk = watchService.take();
+				for (WatchEvent<?> event : wk.pollEvents()) {
+					
+					final Path changed = (Path) event.context();
+					System.out.println("Modify detected!! : " + changed);
+					if (changed.endsWith(WATCH_FILE)) {
+						System.out.println("\n\n\n============================\n\n\n");
+						System.out.println(WATCH_DIRECTORY + File.separator + WATCH_FILE + " Changed");
+						System.out.println("============================\n\n\n");
+						//TODO
+						Main.changeSortInstance();
+					}
+				}
+				
+				// Reset the key
+				boolean valid = wk.reset();
+				if (!valid) {
+					System.out.println("Key has been unregisterede");
+				}
+			}
+			
+		} catch (Exception e) {
+			isSuccess = false;
+			e.printStackTrace();
+		} 
+		
+		isOperation = isSuccess;
+		return isSuccess;
+	}
+	
+	@Override
+	public void run() {
+		watchStart();
 	}
 }
--- IOCPractice/src/Main.java
+++ IOCPractice/src/Main.java
@@ -1,27 +1,75 @@
-
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+	
+	private static Sort instance;
+	private static FileSystemWatcher watcher;
 	
 	public static void main(String[] args) {
 		
 		int[] array = new int[]{3,2,5,2,1,7,8};
-		Sort instance;
+		watcher = FileSystemWatcher.getInstance();
+		Scanner scanner = null;
+		
 		try {
 			instance = SortFactory.getInstance();
-			array = instance.sort(array);
+			watcher.start();
 			
-			new FileSystemWatcher();
+			scanner = new Scanner(System.in);
+			
+			String inputData = null;
+			while(true){
+				System.out.println("\n\n\t[ new Start ]");
+				System.out.println("\nPlease Input Enter, start \""+ instance.getClass().getName() + "\"\n");
+				
+				System.out.print("Before Sort : ");
+				shuffleArray(array);
+				printArray(array);
+				
+				inputData = scanner.nextLine();
+				
+				if (inputData.equalsIgnoreCase("exit"))
+					break;
+
+				array = instance.sort(array);
+				
+				System.out.print("After Sort : ");
+				printArray(array);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		System.out.println("Terminated");
+		scanner.close();
+		watcher.watchStop();
+	}
+	
+	private static void shuffleArray(int[] array) {
+		Random random = new Random();
+	    for (int i = array.length - 1; i > 0; i--)
+	    {
+	      int index = random.nextInt(i + 1);
+
+	      int temp = array[index];
+	      array[index] = array[i];
+	      array[i] = temp;
+	    }
+	}
+
+	private static void printArray(int[] array) {
+		System.out.print("[");
 		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
-		}
-		
-		
-		while(true){
+			System.out.print(array[i]);
 			
+			if (i != array.length-1) {
+				System.out.print(", ");
+			}
 		}
+		System.out.println("]");
+	}
+
+	static void changeSortInstance() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+		instance = SortFactory.getInstance();
 	}
 }
